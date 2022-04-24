d80c38b19dac6a3ceee6b9c407017486780d87c8
Luke Moyer <68236193+doulosinsou@users.noreply.github.com>
2022-04-24 02:21:05 -0500

    Update INSERTS.sql

--- sql files/INSERTS.sql
+++ sql files/INSERTS.sql
@@ -67,7 +67,19 @@ VALUES ('Dark-narrow', '#323538', '#fe9815', '600px', 'Times');
 --- Inserts for Post --- 
 
 INSERT INTO Post (title, page_type, content)
-VALUES ('Home','main','<h1>Website Builder by Luke Moyer</h1><p>This Project creates a MySQL website database and serves it through Flask.</p><p>Follow the <a href="">Project</a> Category to read about the creation process.</p>');
+VALUES ('Home','main',"<h1>Website Builder by Luke Moyer</h1><p>This Project creates a MySQL website database and serves it through Flask.</p><p>Follow the <a href='/topic/Project_full'>Project_full</a> topic to read about the creation process.</p>
+<p>Known bugs:</p>
+<ol><li>The pages have inconsistent theme properties. This not a bug so much as randomized design to show that individual pages(posts) can have unique formatting</li>
+<li>External links do not always render correctly. This is a Flask routing issue that needs further exploration. Typical answers include using url_for() or prepending https://. These are so far unsuccessful. I choose to leave them as-is for now and pursue an answer before production </li>
+<li>Serving Images runs into problems. This is a Flask routing concern to research.</li>
+<li>On the posts, there is a “Previous” and “Next” link to take a reader through the blog. There is an issue with Python's Mysql.connector rendering the request properly. From Workbench, the same query successfully provides the title which will take the reader to the next or previous page in the topic order. However, I cannot get the Python constructors to replicate this. I will need to find a solution or alternative before production. </li>
+<li>On the Search page, using “enter” resets the page with a Get request. This is a matter of disabling the form action with javascript. I haven't had time to fix this.</li>
+<li>On the Search page, the previews render raw html formatting. It would be good to insert a new 'Preview' Column to the Post table, to allow the author to manually type a raw text description or first sentences. This would be used for RSS feeds as well.</li>
+<li>On the Dashboard, the KPI section does not adjust to parameters. This is due to the way the python sql function was built, and I did not have time to update it. The same is true for the Author's individual KPI's which use a view. I would need to recreate the view with the appropriate parameters. </li>
+<li>On the Dashboard, the Author's engagement sparklines are individually scaled, and do not correspond to one another. </li>
+<li>On the Dashboard, using the 'unique' filter sometimes does not appear to do anything. This is because the data is not quite as tailored for this. In a real-world production, the difference between unique visitors and return visitors is more obvious. </li>
+<li>On the Project_full topic, some of the code blocks renterd with xml remove the tags, as they are interpreted literally in the HTML rendering.</li>
+<li>On the Project_full topic, The TEMPLATES page distorts colors of the text, due to literal HTML rendering. Changing colors with developer tool is available. I will need to find a way to render the <pre>pre-code</pre> blocks as straight text..</li></ol>");
 
 INSERT INTO Post (title,subtitle, content)
 VALUES ('Welcome','','<p>This Category will describe the creation process of this website builder</p>');
